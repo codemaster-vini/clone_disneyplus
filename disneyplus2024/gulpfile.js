6ae4a7907f5e41daae53368fc46ec325da008f99
viniciusdev <marco2002rocha@gmail.com>
2024-10-22 20:18:13 -0300

    adicionando o clone do disney plus versão 2024

--- disneyplus2024/gulpfile.js
+++ disneyplus2024/gulpfile.js
@@ -8,14 +8,14 @@ function compilador() {
         .pipe(gulp.dest('./build/styles'));
 };
 
-function compressao() {
-    return gulp.src('./source/images/**/*')
+function comprimir() {
+    return gulp.src('./source/images/*')
         .pipe(imagemin())
         .pipe(gulp.dest('./build/images'));
 };
 
-exports.default = gulp.parallel(compilador, compressao);
+exports.default = comprimir;
 
 exports.watch = function() {
-    gulp.watch('./source/styles/*.scss', gulp.parallel(compilador));
+    gulp.watch('./source/styles/*', gulp.series(compilador));
 };
